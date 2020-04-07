e06975b574ea6661adef14bd765e59fa33013cfc
Tumo M <tumomasire@gmail.com>
2020-04-07 17:56:20 +0200

    Hello World Index Route

---
+++ app.js
@@ -0,0 +1,33 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser")
+var path = require('path');
+
+var indexRoutes = require("./routes/index");
+
+let dbURL = process.env.DATABASEURL || "dbString";
+
+var port = process.env.PORT || 3000; 
+
+// Setup Express/App
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', path.join(__dirname, '/views'));
+
+// Setup Persistance
+app.use(require("express-session")({
+    secret: process.env.SECRET || "swag",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Required Routes
+app.use("/",indexRoutes);
+
+
+app.listen(port, function () {
+    console.log(`Rona-Tracker Server running on ${port}`);
+})

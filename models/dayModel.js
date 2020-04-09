ec6fd4ec81d90175b8b942b9e13554a8995d38ed
Tumo Masire <tumomasire@gmail.com>
2020-04-09 19:21:12 +0200

    Parsing Table 2 is good. All province inf being saved. Save to Day next
    Signed-off-by: Tumo M <tumomasire@gmail.com>

---
+++ models/dayModel.js
@@ -0,0 +1,15 @@
+class Day{
+    provinces = {***REMOVED***;
+    tests = 0;
+    totalSick = 0;
+    date = null;
+
+    constructor(provinces = {***REMOVED***, tests=0, totalSick=0, date=null) {
+      this.provinces = provinces;
+      this.tests = tests;
+      this.totalSick = totalSick;
+      this.date = date;
+  ***REMOVED***
+***REMOVED***
+
+module.exports = Day
---
+++ models/provinceModel.js
@@ -0,0 +1,24 @@
+class Province {
+  name; // String
+  sick; // int
+  dead; // Dead
+  totalDead; // Dead
+  recovered; // Recovered
+  date; // Date
+  men; // int
+  women; // int
+
+  constructor(name, sick=0, dead=[], recovered=0) {
+    this.name = name;
+    this.sick = sick;
+    this.dead = [];
+    this.recovered = recovered;
+    this.date = null;
+    this.men = 0; // Dead
+    this.women = 0; // Dead
+    this.totalDead = 0; // Dead
+***REMOVED***
+***REMOVED***
+module.exports = Province
+
+

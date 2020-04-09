6533979e32134ea6731d512da004d2bb65040bd7
Tumo M <tumomasire@gmail.com>
2020-04-09 19:21:12 +0200

    Parsing Table 2 is good. All province inf being saved. Save to Day next
    Signed-off-by: Tumo M <tumomasire@gmail.com>

--- test/parsing.js
+++ test/parsing.js
@@ -1,9 +1,28 @@
 const HTMLParser = require('node-html-parser'),
-    rp = require('request-promise');
+    rp = require('request-promise'),
+    Province = require("../models/provinceModel"),
+    Death = require("../models/deathModel"),
+    Day = require("../models/dayModel");
 
 const url = "https://sacoronavirus.co.za/category/press-releases-and-notices/";
 const linkRegex = /.*\d{4***REMOVED***\/\d{2***REMOVED***\/\d{2***REMOVED***\/update-.*covid-.*20\d{2***REMOVED***\//
 const regex = RegExp('.*\d{4***REMOVED***\/\d{2***REMOVED***\/\d{2***REMOVED***\/update-.*covid-.*20\d{2***REMOVED***\/', 'g');
+
+let testCount = 0;
+
+const PROVINCES = { // Name, [sick, dead]
+                    "GAUTENG": Province,
+                    "WESTERN CAPE": Province,
+                    "KWAZULU–NATAL": Province,
+                    "FREE STATE": Province,
+                    "EASTERN CAPE": Province,
+                    "LIMPOPO": Province,
+                    "MPUMALANGA": Province,
+                    "NORTH WEST": Province,
+                    "NORTHERN CAPE": Province,
+                    "UNALLOCATED": Province
+                  ***REMOVED***
+let provincesList = []
 
 rp(url)
     .then(function (html) {
@@ -11,18 +30,19 @@ rp(url)
         console.log("success!!");
         //   console.log(html);
         const root = HTMLParser.parse(html);
-        //console.log("Root",root);
 
-        //console.log(root.structure);
         var entries = root.querySelectorAll(".entry-title.fusion-post-title a");
         var links = []
-        entries = entries.filter((entry) => {
+
+        entries.map((entry) => {
             // return entry.getAttribute("href").match(linkRegex)
             if (entry.getAttribute("href").match(linkRegex)) {
                 // console.log("Found One!\n", entry.text);
                 // console.log("Outer: ", entry.outerHTML);
                 // console.log("Inner: ", entry.innerHTML);
                 // console.log("Href: ", entry.getAttribute("href"), "\n");
+
+                const DATE = entry.text.split("(")[1].split(")")[0];
                 links.push(entry.getAttribute("href"))
                 rp(entry.getAttribute("href"))
                     .then(function (html) {
@@ -30,17 +50,17 @@ rp(url)
                         // pull out the two tables 1st
                         const tables = rootChild.querySelectorAll("table");
                         const [table1, table2] = tables;
-                        // ToDo: Handle these 2 tables for Provincial Data.
 
                         // Creates a new root from both tables
                         const rootTable1 = HTMLParser.parse(table1.outerHTML);
-                        const rootTable2 = HTMLParser.parse(table2.outerHTML);
 
                         // Extracts the rows from each table
                         const rowsTable1 = rootTable1.querySelectorAll("tr")
-                        const rowsTable2 = rootTable2.querySelectorAll("tr")
-                        // console.log(rowsTable1);
                         
+                        // console.log(rowsTable1);
+                        console.log(DATE); // date
+                        let currentProvinces = Object.assign({***REMOVED***, PROVINCES);
+
                         rowsTable1.forEach((row) =>{
                             row = row.text.split(" ");
                             let name = "";
@@ -54,10 +74,51 @@ rp(url)
                               ***REMOVED***
                           ***REMOVED***)
                             name = name.trim();
-                            name = name == "KWAZULU – NATAL"? "KWAZULU–NATAL":name
-                            console.log(name,"-",count);
-                      ***REMOVED***)
+                            name = name.match(/(KWAZULU)(\s?)+(.*)+(\s?)+(NATAL)/)? "KWAZULU–NATAL":name;
+                            let currentProvince = new Province(name,count);
+                            currentProvince.date=DATE;
+                            // Adds the province to the list
+                            currentProvinces[name] = currentProvince;
 
+
+                            // console.log(name,"-",count);
+                      ***REMOVED***)
+                        // put table 2 parse here
+
+                        const rootTable2 = HTMLParser.parse(table2.outerHTML);
+                        const rowsTable2 = rootTable2.querySelectorAll("tr")
+                        delete rowsTable2[0]
+                        rowsTable2.forEach(row => {
+                            row = row.text.split(" ");
+                            row.shift();
+                            let name = "";
+                            let count = 0;
+                            let gender = "";
+                            row.forEach((index) =>{
+                                if (index.match(/^\d+$/)){ // Checks that index value is a digit
+                                    count = Number(index);
+                              ***REMOVED***
+                                else if (index !== "FEMALE" && index !== "MALE"){
+                                    name += index+" "; // Appends word and adds a space that was removed from split
+                              ***REMOVED***
+                                else{
+                                    gender = index;
+                              ***REMOVED***
+                          ***REMOVED***)
+                            name = name.trim();
+                            name = name.match(/(KWAZULU)(\s?)+(.*)+(\s?)+(NATAL)/)? "KWAZULU–NATAL":name;
+                            const death = new Death(name,gender,count)
+                            // console.log("Death:",death.toString());
+                            currentProvinces[name].dead.push(death)
+                            gender === "MALE"? currentProvinces[name].men += 1: gender === "FEMALE"? currentProvinces[name].women += 1: console.log("INVALID GENDER");
+                            currentProvinces[name].totalDead = currentProvinces[name].men + currentProvinces[name].women
+
+                      ***REMOVED***)
+                        console.log("Done Table 2")
+
+                        provincesList.push(currentProvinces)
+                        console.log(JSON.stringify(currentProvinces,null,2));
+                        
                         // pull out paragraph after 1st table
                         const tags = rootChild.querySelector(".post-content").childNodes;
 
@@ -89,7 +150,9 @@ rp(url)
           ***REMOVED***
       ***REMOVED***);
         // Pull the stats off a URL
+
   ***REMOVED***)
     .catch(function (err) {
         //handle error
   ***REMOVED***);
+console.log("ProvincesList:",JSON.stringify(provincesList,null,2));

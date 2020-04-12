4f5afc928a484b2a21c773959fce814a65ad0167
Tumo M <tumomasire@gmail.com>
2020-04-12 23:21:07 +0200

    Pulling Recovery Data
    Signed-off-by: Tumo M <tumomasire@gmail.com>

--- test/news24Parser.js
+++ test/news24Parser.js
@@ -6,7 +6,6 @@ const HTMLParser = require('node-html-parser'),
     DbSetup = require("../test/db"),
     JSSoup = require('jssoup').default;;
 const url = "https://www.health24.com/Medical/Infectious-diseases/Coronavirus/coronavirus-in-sa-all-the-confirmed-cases-20200312";
-const linkRegex = /.*\d{4***REMOVED***\/\d{2***REMOVED***\/\d{2***REMOVED***\/update-.*covid-.*20\d{2***REMOVED***\//
 
 const knex = require('knex')({
     client: 'pg',
@@ -36,11 +35,48 @@ let totalCases = 0, totalDeaths = 0
 rp(url)
     .then(function (html) {
         //success!
-        const headBody = html.match(/<h6.*block datestamp[\s\S]*<\/hgroup>/m) // The Header with total Cases and Deaths.
-        const mainBody = html.match(/.*those details can be found here/gmiu) // The body with province counts.
-        const soup = new JSSoup(html);
-        console.log(soup.toString())
+        // #######################################################################################
+        const headHtml = html.match(/<h6.*block datestamp[\s\S]*<\/hgroup>/m) // The Header with total Cases and Deaths.
+        const bodyHtml = html.match(/.*those details can be found here/gmiu) // The body with province counts.
+        const soupHead = new JSSoup(headHtml);
+        const soupBody = new JSSoup(bodyHtml);
+
+
+        // Parses the 'head' of the page for Case and Death Numbers
+        let templines = soupHead.prettify().trim().split(/<\/?\w*\s?\d?[\s\S]?>\n/);
+        let date = templines[0].split('Updated')[1].trim(),
+            intString = "";
+        [totalCases, totalDeaths] = (templines[5].trim().split("."))
+
+        totalCases = getNumber(totalCases)
+        totalDeaths = getNumber(totalDeaths)
+
+        console.log(`${date***REMOVED***: Cases:${totalCases***REMOVED***, Deaths:${totalDeaths***REMOVED***`)
+        // #######################################################################################
+
+        // TODO 1: Parse info on Recoveries by Province
+        let recoveriesLines = soupBody.find('p').getText().split(":")
+        let recoveryDate = recoveriesLines[0].match(/\d+.*/)
+        let RecoveryNumber = getNumber(recoveriesLines[1])
+        let provinceRecoveries = recoveriesLines[2].split(/\),?\.?/)
+        provinceRecoveries.pop() // Removes trailing blank index.
+        provinceRecoveries.forEach(line => {
+            [provinceName, recoverCount] = line.trim().split("(")
+            console.log(`${provinceName***REMOVED***:${recoverCount***REMOVED***`)
+      ***REMOVED***)
+        // TODO 2: Parse info on New Cases by Province
+
+        console.log("Swag 2")
   ***REMOVED***)
     .catch(function (err) {
         console.log(err)
   ***REMOVED***);
+
+function getNumber(line){
+    let intString = "";
+    let total = line.match(/\d[\d+\s+]*\d+/)[0].split(" ")
+    total.forEach(digit => {
+        intString += digit
+  ***REMOVED***)
+    return (+intString)
+***REMOVED***

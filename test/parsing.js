f48dac4842e2692b2d4d98cc85255fb269eb0107
Tumo Masire <tumomasire@gmail.com>
2020-04-13 16:52:42 +0200

    Ready to start on FrontEnd
    Signed-off-by: Tumo M <tumomasire@gmail.com>

--- test/parsing.js
+++ test/parsing.js
@@ -267,6 +267,7 @@ rp(url)
                                                 console.log("Unknown error:",reason)
                                             })
                                         } catch (e) {
+                                            console.log("ANOOOOTHER ERRROR?:",e)
                                             knex("dates ").insert({date: d, parsed: false})
                                                 .then(id => {
                                                     //console.log(id)
@@ -274,7 +275,7 @@ rp(url)
                                                 .catch(err => {
                                                     console.log("Day Error 2")
                                                 })
-                                            throw 'No post?\n'
+                                            throw 'No posts?\n'
                                         }
                                     } catch (e) {
                                         console.log("SOME ERROR:",e)

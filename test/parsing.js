771ca7b8ea273a7a85d1443e3cb3cee7cd15be30
Tumo M <tumomasire@gmail.com>
2020-04-17 01:45:29 +0200

    Updating dates with total test numbers.
    Signed-off-by: Tumo M <tumomasire@gmail.com>

--- test/parsing.js
+++ test/parsing.js
@@ -33,6 +33,17 @@ const PROVINCES = { // Name, [sick, dead]
 ***REMOVED***
 let provincesList = []
 
+function parseNumber(number) {
+    let testInt = "";
+    console.log("FUNC NUMBER:",number)
+    let testArray = number.match(/\s?((\d+\s+)*\d+)/)[0].trim().split(" ");
+    testArray.forEach(digit => {
+        testInt += digit
+  ***REMOVED***)
+    testInt = parseInt(testInt);
+    return testInt
+***REMOVED***
+
 rp(url)
     .then(function (html) {
         //success!
@@ -61,6 +72,8 @@ rp(url)
                         let totalDeath = 0;
                         let tempDate = DATE.split(" ");
                         const d = new Date(`${tempDate[0].split(/\D+/)[0]***REMOVED***-${tempDate[1]***REMOVED***-${tempDate[2]***REMOVED***`);
+                        let parsedDate = d.toLocaleDateString().split("/")
+                        parsedDate = `${parsedDate[2]***REMOVED***-${parsedDate[0]***REMOVED***-${parsedDate[1]***REMOVED***`
                         knex('dates')
                             .where({date: d***REMOVED***)
                             .then(rows => {
@@ -70,15 +83,12 @@ rp(url)
                               ***REMOVED***
                                 else if (rows.length > 0 && rows[0].maybeValid) {
                                     // Maybe the format is all wrong. Parse another site/source?
-
                                     // Make soup.
                                     const paragraphs = HTMLParser.parse(html)
                                     //console.log(html.match(/total number of.*tests.*\s\d+[\.|\n]/i)[0])
-                                    console.log('\n')
                                     // paragraphs.querySelectorAll("p")[2].text.match(/total number of.*tests.*\s\d+[\.|\n]/i)
                                     let testPar = paragraphs.querySelectorAll("p").find((currentVal,index,arr)=>{
                                         let paragraph = currentVal.text.match(/total number of.*tests.*\s\d+[\.|\n]/i)
-                                        console.log("Paragraph:",paragraph)
                                         if (!paragraph){
                                             return false;
                                       ***REMOVED***
@@ -86,7 +96,27 @@ rp(url)
                                             return true
                                       ***REMOVED***
                                   ***REMOVED***)
-                                    console.log("TestPar",testPar.text)
+                                    console.log("TestPar",testPar.text);
+                                    let totalTests = testPar.text.match(/\s((\d+\s+)*\d+)/);
+                                    if (totalTests){
+                                        totalTests = parseNumber(totalTests[0].trim());
+                                  ***REMOVED***
+                                    console.log("Value:",totalTests);
+                                    knex('dates').select('totalTests')
+                                        .whereNull('totalTests')
+                                        .andWhere({date:parsedDate***REMOVED***)
+                                        .then(rows=>{
+                                            knex('dates').update({totalTests,maybeValid:false***REMOVED***).where({date:parsedDate***REMOVED***)
+                                                .then(value => {
+                                                    console.log("Updated TTs:",value)
+                                              ***REMOVED***)
+                                                .catch(reason => {
+                                                    console.log("Unknown 2:",reason);
+                                              ***REMOVED***)
+                                      ***REMOVED***)
+                                        .catch(reason => {
+                                            console.log("Error putting in Total Tests?",reason)
+                                      ***REMOVED***)
                                     // Pull off data for update (Total tests)
                               ***REMOVED***
                                 else if ((rows.length === 0) || (rows.length > 0 && !rows[0].error)) {
@@ -145,12 +175,7 @@ rp(url)
                                                     return tag;
                                               ***REMOVED***
                                           ***REMOVED***);
-                                            let testInt = "";
-                                            let testArray = tests[0].text.match(/\s((\d+\s+)*\d+)/)[0].trim().split(" ");
-                                            testArray.forEach(digit => {
-                                                testInt += digit
-                                          ***REMOVED***)
-                                            testInt = parseInt(testInt);
+                                            parseNumber(tests[0].text);
                                             const rootTable2 = HTMLParser.parse(table2.outerHTML);
                                             const rowsTable2 = rootTable2.querySelectorAll("tr")
                                             delete rowsTable2[0]
@@ -256,7 +281,7 @@ rp(url)
 
                                             console.log("TESTS:", (tests[0].text).match(/\s((\d+\s+)*\d+)/)[0].trim()); // Matches the string for for the test cases.
                                             // console.log("SEARCH DATE: ",`${tempDate[0].split(/\D+/)[0]***REMOVED***-${tempDate[1]***REMOVED***-${tempDate[2]***REMOVED***`)
-                                            let parsedDate = d.toLocaleDateString().split("/")
+                                            parsedDate = d.toLocaleDateString().split("/")
                                             parsedDate = `${parsedDate[2]***REMOVED***-${parsedDate[0]***REMOVED***-${parsedDate[1]***REMOVED***`
                                             console.log("Parsed Date:",parsedDate)
                                             knex('dates')

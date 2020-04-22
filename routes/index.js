13e33a781fa11fa806ccd09eb416e1a6a4335fbf
Tumo M <tumomasire@gmail.com>
2020-04-23 00:46:35 +0200

    Zapier webhooks
    Signed-off-by: Tumo M <tumomasire@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,16 @@ let connection = process.env.DATABASE_URL || {
 console.log("Connection:",connection)
 const knex = require('knex')({
     client: 'pg',
+    "pool": {
+        "min": 2,
+        "max": 6,
+        "createTimeoutMillis": 3000,
+        "acquireTimeoutMillis": 30000,
+        "idleTimeoutMillis": 30000,
+        "reapIntervalMillis": 1000,
+        "createRetryIntervalMillis": 100,
+        "propagateCreateError": false // <- default is true, set to false
+    },
     connection
 })
 

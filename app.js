a8b5db1bb9bfbef40d6e813251415899e9a1186c
Tumo M <tumomasire@gmail.com>
2020-04-24 04:35:47 +0200

    webhook works to receive from zapier. Not on heroku tho.
    Signed-off-by: Tumo M <tumomasire@gmail.com>

--- app.js
+++ app.js
@@ -21,20 +21,87 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, '/views'));
 
-// Setup Persistance
+/*// Setup Persistance
 app.use(require("express-session")({
     secret: process.env.SECRET || "swag",
     resave: false,
     saveUninitialized: false
-***REMOVED***));
+***REMOVED***));*/
 
+app.use(bodyParser.json());
+
+
+let connection;
+if (process.env.DBMODE && process.env.DBMODE === "herokuDB"){
+    connection = process.env.DATABASE_URL
+***REMOVED***
+else{
+    connection = {
+        host:process.env.AWS_HOST|| process.env.PG_HOST||'127.0.0.1',
+        user:process.env.AWS_USER|| process.env.PG_USER||'test_user',
+        password:process.env.AWS_PASSWORD || process.env.PG_PASS ||'temp_pass',
+        database:process.env.AWS_DB ||process.env.DB_NAME ||'covid-tracker-sa2'
+  ***REMOVED***;
+***REMOVED***
+
+console.log("Connection:",connection)
+const knex = require('knex')({
+        client: 'pg',
+        debug: true,
+        asyncStackTraces: true,
+        acquireConnectionTimeout: 10000,
+        pool: {
+            "min": 2,
+            "max":50,
+            idleTimeoutMillis: 10000,
+            createTimeoutMillis: 10000,
+            acquireTimeoutMillis: 10000,
+***REMOVED*****REMOVED*****REMOVED***
+        connection
+  ***REMOVED***
+)
 
 // Required Routes
+app.set('knex', knex);
+app.use(function(req,res, next) {
+    res.locals.knex = knex;
+    next();
+***REMOVED***);
+
 app.use("/",indexRoutes);
 
 app.get('/loaderio-7d6b780c491333bbfc06f6c5bdc20309.txt',(req,res) =>{
     res.sendFile("loaderio-7d6b780c491333bbfc06f6c5bdc20309.txt");
 ***REMOVED***)
+
+// Government Notification
+app.post('/governmentCheck/:type?',async (req,res) =>{
+    let type = req.params.type? req.params.type:
+        req.get("type")? req.get("type"): null;
+    console.log("##################################################################################")
+    console.log("TYPE:",type)
+    console.log("Auth:",req.headers.authorization)
+    console.log("Data:",req.data)
+    console.log("Body:",req.body)
+    console.log("##################################################################################")
+    if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+        return res.status(401).json({ message: 'Missing Authorization Header' ***REMOVED***);
+  ***REMOVED***
+
+    // verify auth credentials
+    const base64Credentials =  req.headers.authorization.split(' ')[1];
+    const [username, password]  = Buffer.from(base64Credentials, 'base64').toString('ascii').split(":");
+    console.log("Body:",username,password)
+
+    if (username===process.env.ZAPIER_USER && password===process.env.ZAPIER_PASSWORD) {
+        return res.status(200).json({ message: 'Valid Authentication Credentials' ***REMOVED***);
+  ***REMOVED***
+    else{
+        return res.status(401).json({ message: 'Invalid Authentication Credentials' ***REMOVED***);
+  ***REMOVED***
+
+***REMOVED***)
+
 
 app.listen(port, function () {
     console.log(`Rona-Tracker Server running on ${port***REMOVED***`);
--- routes/index.js
+++ routes/index.js
@@ -2,48 +2,9 @@ var express = require("express");
 var router = express.Router();
 const numeral = require('numeral');
 
-let connection;
-if (process.env.DBMODE && process.env.DBMODE === "herokuDB"){
-    connection = process.env.DATABASE_URL
-***REMOVED***
-else{
-    connection = {
-        host:process.env.AWS_HOST|| process.env.PG_HOST||'127.0.0.1',
-        user:process.env.AWS_USER|| process.env.PG_USER||'test_user',
-        password:process.env.AWS_PASSWORD || process.env.PG_PASS ||'temp_pass',
-        database:process.env.AWS_DB ||process.env.DB_NAME ||'covid-tracker-sa2'
-  ***REMOVED***;
-***REMOVED***
 
 
-console.log("Connection:",connection)
-const knex = require('knex')({
-    client: 'pg',
-    ssl:true,
-    connection: connection,
-    debug:true,
-    log:true,
-    asyncStackTraces:true,
-    acquireConnectionTimeout: 3000,
-    pool: {
-        min:2,
-        max:100,
-        afterCreate: function (conn, done) {
-            // in this example we use pg driver's connection API
-            conn.query('SET statement_timeout TO "15s";', function (err) {
-                if (err) {
-                    // first query failed, return error and don't try to make next query
-                    console.log("Timeout NOT set")
-                    done(err, conn);
-              ***REMOVED*** else {
-                    // do the second query...
-                    console.log("Timeout set")
-                    done(err, conn);
-              ***REMOVED***
-          ***REMOVED***);
-      ***REMOVED***
-  ***REMOVED***
-***REMOVED***)
+
 
 const provinceList = {
     "NORTH WEST": 'ZA-NW',
@@ -60,92 +21,156 @@ const provinceList = {
 
 
 router.get("/", function (req, res) {
-    // TODO Load data for the day.
-    console.log("At index")
-    getSummary()
-        .then(value => {
-            value.totalCases = numeral(value.totalCases).format('0,0');
-            value.totalDeaths = numeral(value.totalDeaths).format('0,0');
-            value.totalRecoveries = numeral(value.totalRecoveries).format('0,0');
-            value.totalTests = numeral(value.totalTests).format('0,0');
-            // console.log("Sending these stats:",value)
-            getProvinces().then(value1 => {
-                let provCases = {***REMOVED***
-                let provDeaths = {***REMOVED***
-                let provRecoveries = {***REMOVED***
-                value1.forEach(province =>{
-                    provCases[provinceList[province.provinceName.toUpperCase()]] = province.caseCount
-                    provDeaths[provinceList[province.provinceName.toUpperCase()]] = province.deathCount
-                    provRecoveries[provinceList[province.provinceName.toUpperCase()]] = province.recovered
-                    // console.log("Province:",province)
-              ***REMOVED***)
-                //
-                // console.log("Prov Cases:",provCases)
-                // console.log("Prov Deaths:",provDeaths)
-                // console.log("Prov Recovs:",provRecoveries)
-                getGraphData().then(graphData => {
-                    // console.log("Graph Data",graphData)
-                    res.render("index",{data:value,provCases,provDeaths,provRecoveries,graphData***REMOVED***);
-              ***REMOVED***)
-          ***REMOVED***)
-                .catch(function (err) {
-                    // Crawling failed...
-                    console.log("We failed my man",err)
-              ***REMOVED***);
-      ***REMOVED***)
-***REMOVED***)
-let getSummary = function() {
-    return knex('dates')
-        .select('date',"totalCases","totalDeaths","totalTests","totalRecoveries")
-        .whereNotNull("totalCases")
-        .whereNotNull("totalDeaths")
-        .orderBy("date",'desc')
-        .limit(1)
-        .then(function(res1) {
-            if (res1[0].totalTests === null){
-                return knex('dates')
-                    .select('date','totalTests')
-                    .whereNotNull('totalTests')
-                    .limit(1)
-                    .orderBy('date','desc')
-                    .then(value => {
-                        res1[0].date2 = value[0].date;
-                        res1[0].totalTests = value[0].totalTests;
-                        return res1[0]
+    const knex = res.locals.knex;
+    if (knex){
+        console.log("found KNEX")
+        // TODO Load data for the day.
+        getSummary(knex)
+            .then(value => {
+                value.totalCases = numeral(value.totalCases).format('0,0');
+                value.totalDeaths = numeral(value.totalDeaths).format('0,0');
+                value.totalRecoveries = numeral(value.totalRecoveries).format('0,0');
+                value.totalTests = numeral(value.totalTests).format('0,0');
+                // console.log("Sending these stats:",value)
+                getProvinces(knex).then(value1 => {
+                    let provCases = {***REMOVED***
+                    let provDeaths = {***REMOVED***
+                    let provRecoveries = {***REMOVED***
+                    value1.forEach(province =>{
+                        provCases[provinceList[province.provinceName.toUpperCase()]] = province.caseCount
+                        provDeaths[provinceList[province.provinceName.toUpperCase()]] = province.deathCount
+                        provRecoveries[provinceList[province.provinceName.toUpperCase()]] = province.recovered
+                        // console.log("Province:",province)
                   ***REMOVED***)
+                    //
+                    // console.log("Prov Cases:",provCases)
+                    // console.log("Prov Deaths:",provDeaths)
+                    // console.log("Prov Recovs:",provRecoveries)
+                    getGraphData(knex).then(graphData => {
+                        // console.log("Graph Data",graphData)
+                        res.render("index",{data:value,provCases,provDeaths,provRecoveries,graphData***REMOVED***);
+                  ***REMOVED***)
+              ***REMOVED***)
+                    .catch(function (err) {
+                        // Crawling failed...
+                  ***REMOVED***);
+          ***REMOVED***)
+  ***REMOVED***
+    else{
+        console.log("No Knex?")
+  ***REMOVED***
+
+***REMOVED***)
+let getSummary = function(knex) {
+
+    return knex.transaction((trx) =>{
+        return knex('dates')
+            .select('date',"totalCases","totalDeaths","totalTests","totalRecoveries")
+            .whereNotNull("totalCases")
+            .whereNotNull("totalDeaths")
+            .orderBy("date",'desc')
+            .limit(1)
+            .transacting(trx)
+            .then(function(res1) {
+                console.log("Done Summary 1")
+
+                if (res1[0].totalTests === null){
+                    return knex('dates')
+                        .select('date','totalTests')
+                        .whereNotNull('totalTests')
+                        .limit(1)
+                        .orderBy('date','desc')
+                        .transacting(trx)
+                        .then(value => {
+                            res1[0].date2 = value[0].date;
+                            res1[0].totalTests = value[0].totalTests;
+                            console.log("Done Summary")
+                            // return res1[0];
+                            let promise = res1[0];
+                            return promise ;
+                      ***REMOVED***)
+                        .then(trx.commit)
+                        .catch(trx.rollback);
+              ***REMOVED***
+                else {
+                    console.log("Done Summary 3")
+                    trx.commit
+                    let promise = res1[0];
+                    return promise
+              ***REMOVED***
+          ***REMOVED***)
+            .then(trx.commit)
+            .catch(reason => {
+                console.log("You messed up 1?",reason)
+                trx.rollback
+          ***REMOVED***);
+  ***REMOVED***)
+
+
+***REMOVED***
+
+
+/*
+knex.transaction((trx) => {
+    return knex('tab1')
+        .update({ col2: 'val2' ***REMOVED***)
+        .where({ col1: 'val1' ***REMOVED***)
+        .transacting(trx)
+        .then((result) => {
+            let promise;
+            if (result1 != 0) {
+                promise = util.insert(data1);
+          ***REMOVED*** else {
+                promise = util.mark(data2);
           ***REMOVED***
-            else {
-                return res1[0];
-          ***REMOVED***
+            return promise.transacting(trx);
       ***REMOVED***)
-        .catch(reason => {
-            console.log("You messed up?",reason)
-      ***REMOVED***);
+        .then(trx.commit)
+        .catch(trx.rollback)
+***REMOVED***)
+    .then(() => {
+        // blabla
+  ***REMOVED***)
+    .catch((err) => {
+        // handle your error together
+  ***REMOVED***);
+*/
+
+let getProvinces = function(knex) {
+    return knex.transaction((trx) => {
+        return knex('provinceDays')
+            .select("provinceName", "provDate", "caseCount", "deathCount", "recovered")
+            .orderBy("provDate", 'desc')
+            .limit(10)
+            .transacting(trx)
+            .then(function (res) {
+                return res
+          ***REMOVED***)
+            .then(trx.commit)
+            .catch(reason => {
+                console.log("You messed up 2?", reason)
+                trx.rollback
+          ***REMOVED***);
+  ***REMOVED***)
 ***REMOVED***
-let getProvinces = function() {
-    return knex('provinceDays')
-        .select("provinceName","provDate","caseCount","deathCount","recovered")
-        .orderBy("provDate",'desc')
-        .limit(10)
-        .then(function(res) {
-            return res
-      ***REMOVED***)
-        .catch(reason => {
-            console.log("You messed up?",reason)
-      ***REMOVED***);
+
+let getGraphData = function(knex) {
+    return knex.transaction((trx) => {
+        return knex('dates')
+            .select("date", "totalCases",
+                "totalDeaths", "totalRecoveries",
+                "activeCases", "totalTests",
+                "dailyNew", "dailyDeaths")
+            .orderBy("date", 'asc')
+            .then(function (res) {
+                return res
+          ***REMOVED***)
+            .then(trx.commit)
+            .catch(reason => {
+                console.log("You messed up 3?", reason)
+                trx.rollback
+          ***REMOVED***);
+  ***REMOVED***)
 ***REMOVED***
-let getGraphData = function() {
-    return knex('dates')
-        .select("date","totalCases",
-            "totalDeaths","totalRecoveries",
-            "activeCases","totalTests",
-            "dailyNew","dailyDeaths")
-        .orderBy("date",'asc')
-        .then(function(res) {
-            return res
-      ***REMOVED***)
-        .catch(reason => {
-            console.log("You messed up?",reason)
-      ***REMOVED***);
-***REMOVED***
+
 module.exports = router;

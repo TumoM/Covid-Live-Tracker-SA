63c0a07a1b7068506ee0bb893ddf9e07e720df73
Tumo M <tumomasire@gmail.com>
2020-04-30 02:03:37 +0200

    merge issues.
    Signed-off-by: Tumo M <tumomasire@gmail.com>

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -316,7 +316,9 @@ function populateSideCards(cardList){
     let cards = $("#mapContainer .card");
     let figures;
 
+    console.log('Cards',cards)
     cardList.forEach(item=>{
+        console.log('Counter',counter)
         // the html block containing the spans and br tags
         figures = cards[counter].children[1].children[0].children;
         cards[counter].firstElementChild.firstElementChild.innerText=item.name // sets Card Heading.

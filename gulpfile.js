cf2a03e37becc925f2d69c7b534005a49f5b0661
Tumo M <tumomasire@gmail.com>
2020-05-02 04:11:36 +0200

    bundle seems a'ight.
    Signed-off-by: Tumo M <tumomasire@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -43,6 +43,7 @@ gulp.task('sass', function () {
       // Auto-prefix css styles for cross browser compatibility
       // Output
       .pipe(postcss([ autoprefixer() ]))
+      .pipe(cleanCss())
       .pipe(gulp.dest('assets/css'));
 });
 
@@ -61,7 +62,7 @@ gulp.task('pack-js', function () {
 });
 
 gulp.task('pack-css', function () {
-  return gulp.src(['assets/css/*.css', 'assets/css/**/*.css'])
+  return gulp.src(['assets/css/vendor/*.css','assets/css/**/*.css','assets/css/*.css'])
       .pipe(sourcemaps.init())
       .pipe(postcss([pfm()]))
       .pipe(concat('stylesheet.css'))

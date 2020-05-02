bd24d73435c745b7d18a5da7dd5625dce51b1d72
Tumo M <tumomasire@gmail.com>
2020-05-02 04:24:11 +0200

    Enabling cache again
    Signed-off-by: Tumo M <tumomasire@gmail.com>

--- app.js
+++ app.js
@@ -24,19 +24,19 @@ const NodeCache = require('node-cache');
 dotenv.config();
 
 const ttl = 60 * 60 * 1; // cache for 1 Hour
-/*const cache = new CacheService({
+const cache = new CacheService({
     checkperiod: 60,
     maxKeys: 10000,
     stdTTL: ttl,
     useClones: false,
-});*/
-
-const cache = new NodeCache({
-    checkperiod: 60,
-    maxKeys: 10000,
-    stdTTL: 5,
-    useClones: false,
 });
+
+// const cache = new NodeCache({
+//     checkperiod: 60,
+//     maxKeys: 10000,
+//     stdTTL: 5,
+//     useClones: false,
+// });
 
 const port = process.env.PORT || 3000;
 
